c3d6c51338cd1bc2b08a7052fbfaf1f6d2e1362d
Andres Carrasco <andrescm1998@outlook.com>
2022-11-09 11:44:16 +0000

    Use react router

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import {Typography, Button, Box} from '@mui/material'
+import './index.css'
 
 function Home() {
   return (
     <Box id='home'>
         <Typography id='logo-name'>ClueDev</Typography>
         <Typography id='moto'>
-            <span>No merge conflicts.</span>
-            <span>No misscommunication.</span>
-            <span>No stress.</span>
+            <em>Less merge conflicts.</em>
+            <em>No misscommunication.</em>
+            <em>No stress.</em>
         </Typography>
-        <Button id='github-btn'>Sign Up with Github</Button>
+        <Button variant='contained'  id='github-btn'>Sign-Up with Github</Button>
     </Box>
   )
 }
