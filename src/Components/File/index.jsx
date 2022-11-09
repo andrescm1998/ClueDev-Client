bb3394da05b39049d579204ba460a163d29e28b2
GiuliaLeoni <leonig255@gmail.com>
2022-11-09 17:37:42 +0000

    add file component and basic design

--- src/Components/File/index.jsx
+++ src/Components/File/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './index.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder } from "@fortawesome/free-solid-svg-icons";
 
 export const File = () => {
+
+    const folder = <FontAwesomeIcon icon={faFolder}/>;
     return (
         <>
-            <h1>this is a file</h1>
+            <section className='file-container'>
+                {/* file or folder icon */}
+                <span className='icon'>{folder}</span>
+                <h5>File/Folder name</h5>
+                {/* map all collaborators and render counters */}
+                {/* counter component */}
+            </section>
         </>
     )
 }
