3fd19dd332e6f4a0194cd62f33fede33354263bb
vanessa <v.aglagoh@gmail.com>
2022-11-09 14:11:44 +0000

    Created Header and Dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Home from './Components/Home'
+import { Routes, Route } from 'react-router-dom'
+import { Home, Header, Dashboard } from './Components'
 import './App.css'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
+
+      <Route path='/' element={<Header />}>
+        <Route index element={<Dashboard/>}/>
+      </Route>
     </Routes>
   )
 }
