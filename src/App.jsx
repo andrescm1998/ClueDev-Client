936976f72d8bdb6581d956d849d5713a962f4461
GiuliaLeoni <leonig255@gmail.com>
2022-11-10 14:31:43 +0000

    add folder in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-import { Home, Header, Dashboard, Map } from './Components';
+import { Home, Header, Dashboard, Map, Folder } from './Components';
 import './App.css'
-
-import { Folder } from './Components';
 
 function App() {
   return (
