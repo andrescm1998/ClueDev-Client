1da9689200520dfc613b63c4c7a8d947555498d4
Khamarl C <khamarlchristie@hotmail.co.uk>
2022-11-10 11:40:44 +0000

    workspace card template

--- src/Components/Dashboard/index.jsx
+++ src/Components/Dashboard/index.jsx
@@ -1,30 +1,64 @@
 import React from 'react';
+import {useState} from 'react'
 import { Button } from '@mui/material'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import Modal from '../Modal';
+import Counter from '../Counter';
 import './index.css'
 
-const plus = <FontAwesomeIcon icon = {faPlus} />
+const dots = <FontAwesomeIcon icon ={faEllipsis} />
 
 const Dashboard = () => {
+const [toggle, setToggle] = useState(true)
 
+const buttonToggle = () => {
+        setToggle(!toggle)
+
+        
+}
     return (
         <>
             <main className='dash-container'>
                 <section className='header'>
                     <h1>Workspaces</h1>
                     <section className='links'>
-                        <Button className='addBtn'>{plus}</Button>
                         < Modal />
+                        <Button onClick={buttonToggle}>Toggle</Button>
                     </section>
                 </section>
                 <hr/>
                 <section className='workspace-list'>
-                <p>
+                <p className={toggle ? 'workspace-active': 'no-workspace-active' }>
                 There are no workspaces, click the plus icon to add a new workspace
                 </p>
                     {/* <Workspace /> */}
+                    <div className="workspaces-container">
+                        <div className="block">
+                            <div className="block-header">
+                                <h3>Workspace 1</h3>
+                                <span>{dots}</span>
+                            </div>
+                            <span className="counter">< Counter /></span>
+                        </div>
+
+                        <div className="block">
+                            <div className="block-header">
+                                <h3>Workspace 2</h3>
+                                <span>{dots}</span>
+                            </div>
+                            <span className="counter">< Counter /></span>
+                        </div>
+
+                        <div className="block">
+                            <div className="block-header">
+                                <h3>Workspace 3</h3>
+                                <span>{dots}</span>
+                            </div>
+                            <span className="counter">< Counter /></span>
+                        </div>
+                    </div>
+                   
                 </section>
             </main>
 
