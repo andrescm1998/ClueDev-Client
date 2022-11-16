86159a851cabdfb1cc6aeb63a906ff79790afe0a
Andres Carrasco <andrescm1998@outlook.com>
2022-11-16 14:02:03 +0000

    Update redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user';
 import wsReducer from './workspaces';
+import treeReducer from './treeUrl'
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
-        ws: wsReducer,   
+        ws: wsReducer,
+        treeUrl: treeReducer  
     }
 })
